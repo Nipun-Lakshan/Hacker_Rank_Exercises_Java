1497f5f1f91156bc1eebed8d09e440b9ddefb658
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-03-30 14:05:17 +0530

    Question 03

---
+++ Question03.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Question03 {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Input Interger 01 :- ");
+            int a = scan.nextInt();
+            System.out.print("Input Interger 02 :- ");
+            int b = scan.nextInt();
+            System.out.print("Input Interger 03 :- ");
+            int c = scan.nextInt();
+
+            System.out.println("");
+
+            System.out.println("Integer 01 :- " + a);
+            System.out.println("Integer 02 :- " + b);
+            System.out.println("Integer 03 :- " + c);
+        }
+    }
+}

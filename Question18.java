99b37d1a0a8c6689882272bf2f05aa700994722d
Nipun-Lakshan <nipunlakshan81@gmail.com>
2026-03-25 00:09:32 +0530

    Question 18

---
+++ Question18.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Question18 {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i < ((s.length()) - k + 1); i++) {
+            String current = s.substring(i, (i + k));
+
+            if (current.compareTo(largest) > 0) {
+                largest = current;
+            }
+
+            if (current.compareTo(smallest) < 0) {
+                smallest = current;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
